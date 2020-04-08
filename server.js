fc49d3e897cd990ccb118249dbcec5d2eb479a96
Jonathan Watson <jonathanwatson@Jonathans-MacBook-Pro.local>
2020-04-08 09:40:12 -0400

    view all cars complete

--- server.js
+++ server.js
@@ -5,8 +5,43 @@ const app = express();
 
 const PORT = process.env.PORT || 8080;
 
+const cars = [
+  {
+    mileage: 100000,
+    color: "Blue",
+    make: "Tesla",
+    model: "Y",
+  },
+  {
+    mileage: 100,
+    color: "Red",
+    make: "Tesla",
+    model: "X",
+  },
+  {
+    mileage: 1000,
+    color: "White",
+    make: "Tesla",
+    model: "3",
+  },
+  {
+    mileage: 1,
+    color: "Black",
+    make: "Tesla",
+    model: "3",
+  },
+];
+
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
+
+app.get("/", function (req, res) {
+  res.render("index");
+});
+
+app.get("/cars", function (req, res) {
+  res.render("cars", { cars: cars });
+});
 
 app.listen(PORT, function () {
   console.log(`App listening on http://localhost:${PORT}`);
