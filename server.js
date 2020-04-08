c22ae7cfcfbf662817a9e8aad2bf35db629d17c4
Jonathan Watson <jonathanwatson@Jonathans-MacBook-Pro.local>
2020-04-08 09:59:20 -0400

    completed post route, added TODOs for MySQL integration

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const exphbs = require("express-handlebars");
 
 const app = express();
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 const PORT = process.env.PORT || 8080;
 
 const cars = [
@@ -44,16 +47,28 @@ app.get("/", function (req, res) {
 });
 
 app.get("/cars", function (req, res) {
+    //TODO: Run SQL query SELECT * FROM cars;
   res.render("cars", { cars: cars });
 });
 
 app.get("/cars/:VIN", function (req, res) {
+
+    // TODO: Run SQL query SELECT * FROM cars WHERE VIN = req.params.vin
   console.log(req.params.VIN);
   for (let i = 0; i < cars.length; i++) {
     if (cars[i].VIN === req.params.VIN) {
       res.render("single-car", cars[i]);
     }
   }
+});
+
+app.post("/cars", function (req, res) {
+
+    // TODO: Run SQL query to INSERT INTO cars table.
+  const newCar = req.body;
+  cars.push(newCar);
+  console.log(cars);
+  res.json({ success: true });
 });
 
 app.listen(PORT, function () {
