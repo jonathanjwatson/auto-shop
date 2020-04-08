ef826cc4d5fbf44a289993463d601946502d3d02
Jonathan Watson <jonathanwatson@Jonathans-MacBook-Pro.local>
2020-04-08 09:50:50 -0400

    single car view and get route with navigation

--- server.js
+++ server.js
@@ -11,24 +11,28 @@ const cars = [
     color: "Blue",
     make: "Tesla",
     model: "Y",
+    VIN: "asdkhjafs9876q2439058",
   },
   {
     mileage: 100,
     color: "Red",
     make: "Tesla",
     model: "X",
+    VIN: "asdkhjafs9876q2439055",
   },
   {
     mileage: 1000,
     color: "White",
     make: "Tesla",
     model: "3",
+    VIN: "asdkhjafs9876q2439056",
   },
   {
     mileage: 1,
     color: "Black",
     make: "Tesla",
     model: "3",
+    VIN: "asdkhjafs9876q2439057",
   },
 ];
 
@@ -41,6 +45,15 @@ app.get("/", function (req, res) {
 
 app.get("/cars", function (req, res) {
   res.render("cars", { cars: cars });
+});
+
+app.get("/cars/:VIN", function (req, res) {
+  console.log(req.params.VIN);
+  for (let i = 0; i < cars.length; i++) {
+    if (cars[i].VIN === req.params.VIN) {
+      res.render("single-car", cars[i]);
+    }
+  }
 });
 
 app.listen(PORT, function () {
